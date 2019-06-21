37ff001f026795eb986e235393b5d48f39fbdc17
Ramiro <rodriguez.colmeiro@gmail.com>
2019-06-21 13:47:33 +0200

    ADDIGN FILE MANAGEMENT TOOLS

--- Execute_utils.py
+++ Execute_utils.py
@@ -12,6 +12,8 @@ import datetime
 import time
 import os
 import sys
+import uuid
+
 
 
 
@@ -19,8 +21,53 @@ import sys
 #--------------- Exexution Functions -----------------------------------------#
 #-----------------------------------------------------------------------------#
 
-def remote_copy(file_flow, working_directory, server_USER, server_IP, server_PORT, file_server_PATH, file_local_PATH, server_KEY='', print_flag = False):
+def remote_copy(file_flow, working_directory, server_USER, server_IP, server_PORT, file_server_PATH, file_local_PATH, server_KEY='', print_flag = False, compress_flag = False):
+    
+    
+    
+    if compress_flag:
+        # Get names
+        file_server_PATH_ORIGINAL = file_server_PATH
+        file_local_PATH_ORIGINAL = file_local_PATH
+        file_server_PATH_ROOT = os.path.split(file_server_PATH_ORIGINAL)[0]
+        file_server_NAME = os.path.split(file_server_PATH_ORIGINAL)[1]
+        file_local_PATH_ROOT = os.path.split(file_local_PATH_ORIGINAL)[0]
+        file_local_NAME = os.path.split(file_local_PATH_ORIGINAL)[1]
 
+        # Copress file to temporal file before sending it
+        uid_name = uuid.uuid4().hex.upper()[0:6]
+        file_server_PATH = file_server_PATH_ORIGINAL+"_"+uid_name+".tar.gz"
+        file_local_PATH = file_local_PATH_ORIGINAL+"_"+uid_name+".tar.gz"
+            
+        if file_flow == 'remote_to_local':
+            prog_list_copy = list()
+            prog_list_copy.append("/usr/bin/ssh")
+            prog_list_copy.append(server_USER+"@"+server_IP)      
+            prog_list_copy.append("-p")
+            prog_list_copy.append(server_PORT)
+            
+            prog_list_copy.append("/bin/tar")
+            prog_list_copy.append("-czvf")
+            prog_list_copy.append(file_server_PATH)
+            prog_list_copy.append("-C")
+            prog_list_copy.append(file_server_PATH_ROOT)
+            prog_list_copy.append(file_server_NAME)
+                       
+            exec_and_print(prog_list_copy, '', working_directory, print_flag)
+            
+        elif file_flow == 'local_to_remote':
+            prog_list_copy = list()
+            prog_list_copy.append("/bin/tar")
+            prog_list_copy.append("-czvf")
+            prog_list_copy.append(file_local_PATH)
+            prog_list_copy.append("-C")
+            prog_list_copy.append(file_local_PATH_ROOT)
+            prog_list_copy.append(file_local_NAME)
+            
+            exec_and_print(prog_list_copy, '', working_directory, print_flag)
+        else:
+            sys.exit("Invalid data flow. Valid strings are: \"remote_to_local\" and \"local_to_remote\" ")
+    
     # Recover the result
     prog_list_copy = list()
     
@@ -41,10 +88,68 @@ def remote_copy(file_flow, working_directory, server_USER, server_IP, server_POR
     else:
         sys.exit("Invalid data flow. Valid strings are: \"remote_to_local\" and \"local_to_remote\" ") 
         
-    
-    
+    # Copy
     output_log = exec_and_print(prog_list_copy, '', working_directory, print_flag)
-    
+        
+        
+    if compress_flag:
+        
+        # Uncompress the file
+        if file_flow == 'remote_to_local':
+            prog_list_copy = list()
+            prog_list_copy.append("/bin/tar")
+            prog_list_copy.append("-xzvf")
+            prog_list_copy.append(file_local_PATH)
+            prog_list_copy.append("-C")
+            prog_list_copy.append(file_local_PATH_ROOT)
+            
+            exec_and_print(prog_list_copy, '', working_directory, print_flag)
+            
+            # Change file name to local given name (if different)
+            if file_server_NAME != file_local_NAME:
+                prog_list_copy = list()
+                prog_list_copy.append("/bin/mv")
+                prog_list_copy.append(os.path.join(file_local_PATH_ROOT,file_server_NAME))
+                prog_list_copy.append(file_local_PATH_ORIGINAL)
+            
+                exec_and_print(prog_list_copy, '', working_directory, print_flag)
+            
+            
+        elif file_flow == 'local_to_remote':
+            prog_list_copy = list()
+            prog_list_copy.append("/usr/bin/ssh")
+            prog_list_copy.append(server_USER+"@"+server_IP)
+            prog_list_copy.append("-p")
+            prog_list_copy.append(server_PORT)
+
+            prog_list_copy.append("/bin/tar")
+            prog_list_copy.append("-xzvf")
+            prog_list_copy.append(file_server_PATH)
+            prog_list_copy.append("-C")
+            prog_list_copy.append(file_server_PATH_ROOT)
+            
+            exec_and_print(prog_list_copy, '', working_directory, print_flag)
+            
+        # Erase on server
+        prog_list_copy = list()
+        prog_list_copy.append("/usr/bin/ssh")
+        prog_list_copy.append(server_USER+"@"+server_IP)
+        prog_list_copy.append("-p")
+        prog_list_copy.append(server_PORT)
+
+        prog_list_copy.append("/bin/rm")
+        prog_list_copy.append(file_server_PATH)
+
+        exec_and_print(prog_list_copy, '', working_directory, print_flag)
+
+        # Erase localy
+        prog_list_copy = list()
+        prog_list_copy.append("/bin/rm")
+        prog_list_copy.append(file_local_PATH)
+
+        exec_and_print(prog_list_copy, '', working_directory, print_flag)
+
+
     return output_log
 
 
@@ -93,15 +198,23 @@ def exec_and_print_2(prog_list, inputs_str, working_directory, print_flag):
 
 def execute_program_2(prog_list, inputs_str, working_directory):
     
+    if(working_directory != '.'):
+        origWD = os.getcwd() # remember our original working directory
+        os.chdir(working_directory)
+
     launched_process = subprocess.Popen(prog_list, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
     
     output, error = launched_process.communicate(inputs_str)
+    
+    if(working_directory != '.'):
+        os.chdir(origWD) # get back to our original working directory
 
     return output
 
-def exec_and_print(prog_list, inputs_str, working_directory, print_flag):
+
+def exec_and_print_1(prog_list, inputs_str, working_directory, print_flag):
     
-    output_log = list()   
+    output_log = list() 
     
     if print_flag:
         for output_line in execute_program(prog_list,inputs_str, working_directory):
@@ -113,6 +226,19 @@ def exec_and_print(prog_list, inputs_str, working_directory, print_flag):
 
         
     return output_log
+
+def exec_and_print(prog_list, inputs_str, working_directory, print_flag):
+    
+    if inputs_str == '':
+        output_log = exec_and_print_1(prog_list, inputs_str, working_directory, print_flag)
+    else:
+        output_log = exec_and_print_2(prog_list, inputs_str, working_directory, print_flag)
+    
+      
+    
+    return output_log
+    
+    
 
 
 
